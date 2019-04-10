95c83c49ac4004583e5b608a3a9d6c8827de58e6
jordynmcginnis <jordynm4@gmail.com>
2019-04-09 21:19:16 -0600

    actions component created

--- src/components/Actions.js
+++ src/components/Actions.js
@@ -3,3 +3,9 @@ import NumberFormat from 'react-number-format';
 import { RadioGroup, RadioButton } from 'react-radio-buttons';
 import { submitExpense, updateBudget } from '../api/index.js';
 import { useAlert } from 'react-alert';
+
+function Actions(props) {
+  return (
+
+  )
+}
