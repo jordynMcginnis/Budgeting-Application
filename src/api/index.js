d3d76fe3b25a52e61ba842805d3eacb3c6eefeb6
jordynmcginnis <jordynm4@gmail.com>
2019-04-09 21:33:21 -0600

    deleteExpense function created

--- src/api/index.js
+++ src/api/index.js
@@ -28,4 +28,15 @@ module.exports = {
         console.log(error);
       })
   },
+  deleteExpenses: () => {
+    const data = {id: 7};
+    return axios.delete('http://localhost:4000/api/delete_expenses',data,axiosParams)
+      .then(function (response) {
+        return response.data;
+      })
+      .catch(function (error) {
+        console.log(error)
+        return error;
+      })
+  },
 };
