6ffdbcc8afe84cbb33cde216631fbff7c4bfc2a6
jordynmcginnis <jordynm4@gmail.com>
2019-04-09 21:15:59 -0600

    expense chart component created

--- src/components/ExpenseChart.js
+++ src/components/ExpenseChart.js
@@ -1,1 +1,10 @@
-ExpenseChart.js
+function ExpenseChart(props) {
+  return (
+    <div className='expenseChart'>
+      <h3> ExpenseChart </h3>
+      <SimpleBarChart expenses={props.expenses}/>
+    </div>
+  );
+}
+
+export default ExpenseChart;
