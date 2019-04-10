fc49712b33976e74440c64bf843c758bec78579a
jordynmcginnis <jordynm4@gmail.com>
2019-04-10 17:55:19 -0600

    updated server router endpoint for budget

--- src/api/server/lib/server_web/router.ex
+++ src/api/server/lib/server_web/router.ex
@@ -11,5 +11,8 @@ defmodule ServerWeb.Router do
     get "/show_posts", ExpenseController, :index
     post "/add_expense", ExpenseController, :create
     delete "/delete_expenses/:id", ExpenseController, :delete
+    put "/update_budget/:id", BudgetController, :update
+    post "/add_budget", BudgetController, :create
+    get "/show_budget", BudgetController, :index
   end
 end
