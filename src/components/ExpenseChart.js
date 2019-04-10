518e9dd658deef6dd160241476fb9fcd1eeb82d2
jordynmcginnis <jordynm4@gmail.com>
2019-04-09 21:16:55 -0600

    barchart component created and rendered by expenseChart

--- src/components/ExpenseChart.js
+++ src/components/ExpenseChart.js
@@ -1,3 +1,39 @@
+import React from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+function SimpleBarChart(props) {
+  const categoryKey = Object.entries(props.expenses.reduce((objKey, obj) => {
+  if(objKey[obj.category]){
+    objKey[obj.category] += obj.price;
+  } else {
+    objKey[obj.category] = obj.price;
+  }
+    return objKey
+  }, {})).map((item)=>{
+    return {category: item[0], price: item[1]}
+  });
+
+  return (
+    <ResponsiveContainer>
+      <BarChart
+        width={500}
+        height={300}
+        data={categoryKey}
+        margin={{
+          top: 5, right: 30, left: 20, bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3"/>
+        <XAxis dataKey="category"/>
+        <YAxis/>
+        <Tooltip/>
+        <Legend/>
+        <Bar dataKey="price" fill="#63c6c4"/>
+      </BarChart>
+    </ResponsiveContainer>
+  )
+}
+
 function ExpenseChart(props) {
   return (
     <div className='expenseChart'>
