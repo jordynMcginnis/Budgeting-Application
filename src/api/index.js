f81b8b96674755994da0b13a918c4388e1597df6
jordynmcginnis <jordynm4@gmail.com>
2019-04-10 17:53:11 -0600

    updated deleteExpenses functions

--- src/api/index.js
+++ src/api/index.js
@@ -31,11 +31,33 @@ module.exports = {
   deleteExpenses: (id) => {
     return axios.delete(`http://localhost:4000/api/delete_expenses/${id}`,axiosParams)
       .then(function (response) {
-        return response
+        return id;
       })
       .catch(function (error) {
         console.log(error)
         return error;
       })
   },
+  getBudget: () => {
+    return axios.get('http://localhost:4000/api/show_budget', axiosParams)
+      .then(function (response) {
+        return response.data;
+      })
+      .catch(function (error) {
+        console.log(error)
+        return error;
+      })
+  },
+  updateBudget: (amount) => {
+    const data = {budget: {budget: amount}};
+    return axios.put('http://localhost:4000/api/update_budget/1', data, axiosParams)
+      .then(function (response) {
+        console.log(response)
+        return response.data
+      })
+      .catch(function (error) {
+        console.log(error)
+        return error;
+      })
+  }
 };
