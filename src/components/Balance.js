90078ef0cb7848bb72b85b3c3dd473c3b6419783
jordynmcginnis <jordynm4@gmail.com>
2019-04-09 21:10:21 -0600

    balance bar created

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -4,6 +4,25 @@ import { useAlert } from 'react-alert';
 import { getBalance } from '../api/index.js';
 
 function Balance(props) {
+  const budgetSpent = props.expenses.reduce((amount, expense) => {
+   return amount += expense.price;
+  },0);
+
+  const alert = useAlert();
+  const percentageSpent = (budgetSpent/(props.budget + budgetSpent))*100;
+  const percentageLeft = 100 - percentageSpent;
+
+  const barStyle = {
+    margin: '20px',
+    width: '80%',
+    height: '20px',
+    background: `linear-gradient(to right,  #63c5c4 ${percentageLeft}%,#f6f6f6 ${percentageSpent}%)`
+  }
+
+  useEffect(() => {
+    alert.show('Update your Budget!');
+    alert.show('Add an Expense');
+  },[]);
 
   return (
     <div className='balance'>
