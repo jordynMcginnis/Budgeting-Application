04f0be6654bdf96678e73dcc648ed892000677aa
jordynmcginnis <jordynm4@gmail.com>
2019-04-09 21:15:10 -0600

    spending insights component created

--- src/components/SpendingInsights.js
+++ src/components/SpendingInsights.js
@@ -1,1 +1,20 @@
-SpendingInsights.js
+import React from 'react';
+import { FaDollarSign, FaCreditCard } from "react-icons/fa";
+
+function SpendingInsights(props) {
+  return (
+    <div className='spending-insights'>
+      <h3>Spending Insights</h3>
+      <ul>
+        {props.expenses.map((expense, index) => (
+          <li key={index}>
+            <span><FaCreditCard className='fa-icon'/>{expense.category}</span>
+            <span><FaDollarSign className='fa-icon'/>{expense.price}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default SpendingInsights;
